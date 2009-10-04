51e9e60a9a686aaf76c1669cdd87f301ead39f6f
mslupny <mslupny@gmail.com>
2009-10-05 00:02:25 +0200

    clean up before rewrite

--- treeview.cpp
+++
@@ -1,8 +0,0 @@
-#include "mainwindow.h"
-#include "treeview.h"
-
-TreeView::TreeView( QWidget* aparent ) : QTreeView( aparent ) {}
-
-QSize TreeView::sizeHint() const {
-    return QSize( 200, this->height() );
-}
